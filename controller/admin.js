b8f4f50eb820436b783ef174bf5e8f07ec2abc88
Aayu-shh <aayush28.official@gmail.com>
2023-06-18 15:59:50 +0530

    Enabled Adding expense to DB and List

--- controller/admin.js
+++ controller/admin.js
@@ -1,12 +1,12 @@
 const db = require('../util/database');
 
 exports.addExpense = (req,res)=>{
-    const amt = req.body.amt;
-    const desc = req.body.desc;
+    const amt = req.body.amount;
+    const desc = req.body.descp;
     const type = req.body.type;
     db.execute('INSERT INTO expenses(amount,descp,type) VALUES(?,?,?)', [amt, desc, type])
     .then(result=>{
-        if(result.affectedRows>0) res.send("Data Inserted successfully");
+       res.send(result);
     })
     .catch(err => console.log(err));
 }
--- main.js
+++ main.js
@@ -5,27 +5,30 @@ const type = document.getElementById('expenseType');
 
 const itemList = document.querySelector('.items');
 
-window.addEventListener('DOMContentLoaded',(e)=>{
+window.addEventListener('DOMContentLoaded', (e) => {
     axios.get('http://localhost:5000/expenses')
-    .then(resultObj => {
-    (resultObj.data).forEach(exp => {
-        showOutput(exp);
-    });
-    })
-    .catch(err => console.log(err));
+        .then(resultObj => {
+            (resultObj.data).forEach(exp => {
+                showOutput(exp);
+            });
+        })
+        .catch(err => console.log(err));
 });
 
-myForm.addEventListener('submit',(e)=>{
+myForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let exp = {
         amount: amt.value,
         descp: desc.value,
         type: type.value
     }
-    showOutput(exp);
+    axios.post('http://localhost:5000/add-expense', exp)
+        .then(() => showOutput(exp))
+        .catch(err=>console.log(err));
+        
 })
 
-function showOutput(res){
+function showOutput(res) {
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
     delBtn.appendChild(document.createTextNode('Delete'))
@@ -34,18 +37,19 @@ function showOutput(res){
     li.appendChild(document.createTextNode(`${res.amount}: ${res.descp} : ${res.type}`))
     delBtn.classList = 'btn btn-danger m-2';
     editBtn.classList = 'btn btn-warning';
-    li.append(delBtn,editBtn);
+    li.append(delBtn, editBtn);
     itemList.append(li);
     let expLocal = {
-        amt: amt.value,
-        desc: desc.value,
-        type: type.value
+        amt: res.amount,
+        desc: res.descp,
+        type: res.type
     }
-    delBtn.onclick = ()=>{
+    delBtn.onclick = () => {
         itemList.removeChild(li);
         console.log(expLocal.desc + ' was deleted!');
     }
-    editBtn.onclick = ()=>{
-        console.log("Edit Button clicked");
+    editBtn.onclick = () => {
+       
+        console.log('Editing:'+ expLocal.desc);
     }
 }
